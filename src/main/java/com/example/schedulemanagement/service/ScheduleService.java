4990eb3e7e67820bd4bc53d1b8e2be3355e0b9a6
BKH <dallas9115@gmail.com>
2026-02-04 20:55:00 +0900

    Feat: Lv 3~4. 일정 수정 및 삭제 기능 구현 (비밀번호 검증 포함)
    - Service: 일정 수정(updateSchedule) 및 삭제(deleteSchedule) 로직 구현
    - Validation: 수정/삭제 시 비밀번호 일치 여부 확인 로직 추가
    - Entity: Schedule 엔티티에 update 메서드 추가 (제목, 작성자만 수정 가능)
    - Controller: PUT, DELETE 요청 매핑
    - Refactor: 불필요한 Entity 필드 수정 로직 제거 (Contents 수정 불가 처리)

--- src/main/java/com/example/schedulemanagement/service/ScheduleService.java
+++ src/main/java/com/example/schedulemanagement/service/ScheduleService.java
@@ -37,4 +37,43 @@ public class ScheduleService {
                 .toList();
     }
     // 수정일 순으로 모든 성분을 가져와 stream에 넣고, DTO의 형태에 맞춰 넣음
+
+    // 일정 수정 기능 구현
+    @Transactional
+    public ScheduleResponseDto updateSchedule(Long id, ScheduleRequestDto requestDto){
+        //해당 일정이 있는지 확인하고, 없으면 에러 반환
+        Schedule schedule = findSchedule(id);
+
+        // 비밀번호 체크 로직
+        // requestDto.getpassword 를 통해 받은 비밀번호와 DB의 비밀번호 불일치 시 에러 반환
+        if (!schedule.getPassword().equals(requestDto.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        // 비밀번호 일치 시 수정 및 수정된 결과 반환
+        schedule.update(requestDto);
+
+        return new ScheduleResponseDto(schedule);
+    }
+
+    public Long deleteSchedule(Long id, ScheduleRequestDto requestDto){
+        // 해당 일정이 있는지 확인, 없으면 에러 반환
+        Schedule schedule = findSchedule(id);
+
+        //위와 같은 비밀번호 체크 로직
+        if (!schedule.getPassword().equals(requestDto.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        scheduleRepository.delete(schedule);
+
+        return id; //삭제한 ID 반환
+
+    }
+
+    private Schedule findSchedule(Long id) {
+        return scheduleRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("선택한 일정이 없습니다.")
+        );
+    }
 }
